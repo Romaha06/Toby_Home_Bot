388c8442ec6721c6c4beb4e544ad60074001753e
RomaChel <romanchelombitko@gmail.com>
2020-03-03 22:13:01 +0200

    Toby_Home_Bot v0.1

---
+++ src/main/java/Model.java
@@ -0,0 +1,48 @@
+public class Model {
+
+    private String name;
+    private Double temp;
+    private Double humidity;
+    private String icon;
+    private String maim;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getTemp() {
+        return this.temp;
+    }
+
+    public void setTemp(Double temp) {
+        this.temp = temp;
+    }
+
+    public Double getHumidity() {
+        return humidity;
+    }
+
+    public void setHumidity(Double humidity) {
+        this.humidity = humidity;
+    }
+
+    public String getIcon() {
+        return this.icon;
+    }
+
+    public void setIcon(String icon) {
+        this.icon = icon;
+    }
+
+    public String getMaim() {
+        return maim;
+    }
+
+    public void setMaim(String maim) {
+        this.maim = maim;
+    }
+}
